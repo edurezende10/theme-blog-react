bc392b78b7db70622a69b6c919d45d0e9b3c5d9d
eduardo rezende cruz <edu_rezende11@hotmail.com>
2021-03-25 16:57:04 -0300

    adicionando funcionalidade da api no main banner

--- src/Components/MainBanner/MainBanner.js
+++ src/Components/MainBanner/MainBanner.js
@@ -1,12 +1,52 @@
-import React from 'react'
-import './MainBanner.css'
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
+import './MainBanner.css';
 
 const MainBanner = () => {
-    return (
-      <section className="main-banner">
-        <h1 className="main-banner__title">MAIN BANNER</h1>
-        <a href="/produto" className="main-banner__btn">Ver mais</a>
-      </section>
-    )
-  }
-  export default MainBanner
+  const { getRandomImgByTerm } = api.Unsplash,
+    defaultImg =
+      'https://i.pinimg.com/originals/65/ba/48/65ba488626025cff82f091336fbf94bb.gif',
+    defaultTerm = 'nature',
+    [isLoading, setIsLoading] = useState(true),
+    [img, setImg] = useState(defaultImg),
+    [term, setTerm] = useState(defaultTerm);
+
+  useEffect(() => {
+    const imgCallback = (url) => {
+      if (!url.includes('source-404')) {
+        setImg(url);
+        setTimeout(setIsLoading(false), 2500);
+      } else {
+        setImg(defaultImg); 
+        setIsLoading(true);
+        changeTerm();
+      }
+    };
+    getRandomImgByTerm(term, imgCallback);
+  }, [getRandomImgByTerm, term]);
+  const changeTerm = (event = undefined) => {
+    if (event !== undefined) {
+      event.preventDefault();
+    }
+    const newTerm = prompt(
+      'Qual tema deseja?\nSepare os temas por vírgula.\nO termo deve ser escrito em inglês',
+    );
+    if (newTerm && newTerm !== undefined) {
+      setTerm(newTerm.replace(/ /g , ''));
+    } else {
+      return;
+    }
+  };
+  return (
+    <section
+      className={`main-banner${isLoading ? ' loading' : ''}`}
+      style={{ backgroundImage: `url(${img})` }}
+    >
+      <h1 className="main-banner__title">MAIN BANNER</h1>
+      <a href="/produto" className="main-banner__btn" onClick={changeTerm}>
+        Trocar termo
+      </a>
+    </section>
+  );
+};
+export default MainBanner;
---
+++ src/services/api/index.js
@@ -0,0 +1,8 @@
+import Unsplash from './Unsplash.js'
+
+
+const api = {
+    Unsplash
+}
+
+export default api
